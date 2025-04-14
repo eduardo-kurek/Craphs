0619af4467b901f2f1d553d4480f65527b31776a
Eduardo Kurek <eduardokurek2003@gmail.com>
2025-04-13 21:53:51 -0300

    Implementing Connected Components

--- include/Paths.h
+++ include/Paths.h
@@ -2,6 +2,9 @@
 
 #include "Visitor.h"
 #include "Search.h"
+#include <unordered_map>
+#include <optional>
+#include <stdexcept>
 #include <concepts>
 #include <cstdint>
 #include <vector>
@@ -13,12 +16,18 @@ class Paths : public Visitor {
 
 private:
     const Graph& graph;
-    const uint32_t startVertex = 0;
-    uint32_t* edgeTo = nullptr;
     T searcher;
+
+    uint32_t* edgeTo = nullptr;
+    uint32_t* startVertexes = nullptr;
+    std::unordered_map<uint32_t, uint32_t> idMap;
+
+    uint32_t lastId = 0;
+    uint32_t currStartVertex;
 
     std::stack<uint32_t> GetPathStack(uint32_t v) const{
         std::stack<uint32_t> pathStack;
+        uint32_t startVertex = GetStartVertex(v).value();
         for(uint32_t x = v; x != startVertex; x = edgeTo[x])
             pathStack.push(x);
         pathStack.push(startVertex);
@@ -34,12 +43,24 @@ private:
         return pathVector;
     }
 
+    void UpdateCurrentStartVertex(uint32_t v){ currStartVertex = v; }
+    
+    void MapId(uint32_t v){
+        if(Searched(v)) UpdateId(v);
+        else NewId(v);
+    }
+
+    void UpdateId(uint32_t v){ idMap[v] = GetId(v).value(); }
+    void NewId(uint32_t v){ idMap[v] = lastId; lastId++; }
+    
+
 public:
-    explicit Paths(const Graph& graph, uint32_t startVertex = 0) 
-        : graph(graph), startVertex(startVertex), searcher(graph)
+    Paths(const Graph& graph) 
+        : graph(graph), searcher(graph)
     {
-        edgeTo = new uint32_t[graph.V()];
-        searcher.Run(*this, startVertex);
+        edgeTo = new uint32_t[graph.V()]();
+        startVertexes = new uint32_t[graph.V()];
+        std::fill(startVertexes, startVertexes + graph.V(), -1);
     }
 
     ~Paths() override{
@@ -47,8 +68,15 @@ public:
         edgeTo = nullptr;
     }
 
+    void Search(uint32_t startVertex){
+        UpdateCurrentStartVertex(startVertex);
+        MapId(startVertex);
+        searcher.Run(*this, startVertex);
+    }
+
     void Receive(uint32_t v, uint32_t w) override{
         edgeTo[w] = v;
+        startVertexes[w] = currStartVertex;
     }
 
     std::vector<uint32_t> PathTo(uint32_t v) const{
@@ -58,9 +86,47 @@ public:
         return GetPathVector(pathStack);
     }
 
+    bool HasPathTo(uint32_t v) const{
+        return searcher.HasPathTo(v);
+    }
+
+    std::optional<uint32_t> GetStartVertex(uint32_t v) const{
+        graph.CheckVertex(v);
+        if(Searched(v))
+            return startVertexes[v];
+        return {};
+    }
+
+    std::optional<uint32_t> GetId(uint32_t v) const{
+        graph.CheckVertex(v);
+        auto startVertex = GetStartVertex(v);
+        if(startVertex.has_value())
+            return idMap.at(startVertex.value());
+        return {};
+    }
+
+    bool Searched(uint32_t v) const{
+        graph.CheckVertex(v);
+        return startVertexes[v] != -1;
+    }
+
     friend std::ostream& operator<<(std::ostream& os, const Paths& paths){
-        for (uint32_t i = 0; i < paths.graph.V(); ++i)
-            os << i << " -> " << paths.edgeTo[i] << std::endl;
+        os << "Vertex -> EdgeTo | (StartVertex, Connected component ID)" << std::endl << std::endl;
+        for(uint32_t i = 0; i < paths.graph.V(); ++i){
+            bool wasSearched = paths.Searched(i);
+            if(!wasSearched){
+                std::cout << i << ": Not searched yet" << std::endl;
+                continue;
+            }
+
+            auto edgeTo = paths.edgeTo[i] == -1 ? "-" : std::to_string(paths.edgeTo[i]);
+            auto startVertex = wasSearched ? std::to_string(paths.GetStartVertex(i).value()) : "-";
+            auto id = wasSearched ? std::to_string(paths.GetId(i).value()) : "-";
+
+            char buffer[128];
+            snprintf(buffer, sizeof(buffer), "%d -> %s | (%s, %s)", i, edgeTo.c_str(), startVertex.c_str(), id.c_str());
+            os << buffer << std::endl;
+        }
         return os;
     }
 
--- main.cpp
+++ main.cpp
@@ -5,27 +5,31 @@
 #include "Paths.h"
 
 int main(){
-    Graph g(6);
+    Graph g(13);
     g.AddEdge(0, 5);
-    g.AddEdge(2, 4);
-    g.AddEdge(2, 3);
-    g.AddEdge(1, 2);
+    g.AddEdge(4, 3);
     g.AddEdge(0, 1);
-    g.AddEdge(3, 4);
-    g.AddEdge(3, 5);
+    g.AddEdge(9, 12);
+    g.AddEdge(6, 4);
+    g.AddEdge(5, 4);
     g.AddEdge(0, 2);
+    g.AddEdge(11, 12);
+    g.AddEdge(9, 10);
+    g.AddEdge(0, 6);
+    g.AddEdge(7, 8);
+    g.AddEdge(9, 11);
+    g.AddEdge(5, 3);
 
     Paths<BreadthFirstSearch> path(g);
+    path.Search(3);
 
-    auto pathTo5 = path.PathTo(5);
-    std::cout << "Path to 1: ";
-    for (const auto& vertex : pathTo5) {
-        std::cout << vertex << " ";
-    }
-    std::cout << std::endl;
+    std::cout << path << std::endl;
+
+    path.Search(1);
 
     std::cout << path << std::endl;
 
     //std::cout << g << std::endl;
+
     return 0;
 }
